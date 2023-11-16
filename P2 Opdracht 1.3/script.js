c1abf59351e794c3ea8ab70c5d8b339dec455e02
Emily <144031038+EmilyVanDenBerg@users.noreply.github.com>
2023-11-16 11:08:43 +0100

    P2 Opdracht 1.3

---
+++ P2 Opdracht 1.3/script.js
@@ -0,0 +1,5 @@
+// Functie om de input op te slaan en in de console op te slaan.
+function saveInput() {
+    var savedInput = document.getElementById("inputProduct").value;
+    console.log(savedInput);
+}
